11a28c5deeabe9b3796d815330deca8ddb5f629e
chrip <chripinto99@hotmail.com>
2022-04-14 15:56:14 +0200

    modifiche file index.html

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var loadFile = function(event) {
+    var image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    let ratio = image.style.width / image.style.height 
+    image.style.height = "200px"
+    image.style.width = ratio * height
+  };
