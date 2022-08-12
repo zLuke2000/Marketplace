e98e3d5f125db658e9c3622dcaa99d99f92f4167
chrip <chripinto99@hotmail.com>
2022-08-12 16:27:13 +0200

    modifiche

--- ganache/ganache.cjs
+++ ganache/ganache.cjs
@@ -1,23 +1,22 @@
-const ganache = require("ganache");
+const ganache = require('ganache');
 
-const options = { wallet: {totalAccounts: "100",
-                           seed: "marketplaceTest",
-                           accountKeysPath: "keys.json"},
-                  server: {host: "192.168.1.56"},
-		  database: {dbPath: "db"}
-                };
+const options = {
+	wallet: { totalAccounts: '100', seed: 'marketplaceTest', accountKeysPath: 'keys.json' },
+	server: { host: '192.168.1.56' },
+	database: { dbPath: 'db' },
+};
 const server = ganache.server(options);
 const PORT = 8545;
-server.listen(PORT, async err => {
-  if (err) throw err;
+server.listen(PORT, async (err) => {
+	if (err) throw err;
 
-  console.log(`ganache listening on port ${PORT}...`);
-  const provider = server.provider;
-  const accounts = await provider.request({
-    method: "eth_accounts",
-    params: []
-  });
-  console.log(server)
-  console.log(provider)
-  console.log(accounts);
+	console.log(`ganache listening on port ${PORT}...`);
+	const provider = server.provider;
+	const accounts = await provider.request({
+		method: 'eth_accounts',
+		params: [],
+	});
+	console.log(server);
+	console.log(provider);
+	console.log(accounts);
 });
