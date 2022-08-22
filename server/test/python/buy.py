186acfcdedfcba2ebfd869ad10603d1b7d1a059d
ubuntu server <>
2022-08-22 09:51:01 +0200

    Sistemati file python per tempo totale sell e buy

--- server/test/python/buy.py
+++ server/test/python/buy.py
@@ -4,7 +4,6 @@ def convert(lines):
     for line in lines:
         data = line.split(',')
         if len(data) == 5:
-            #                -owner-         ------- database  read -------         ---- metamask transaction ----         ------ database update  ------
-            f_out.writelines(data[0] + "," + str(int(data[2])-int(data[1])) + "," + str(int(data[3])-int(data[2])) + "," + str(int(data[4])-int(data[3])) + "\n")
-            #print(data[0], " - database read:", int(data[2])-int(data[1]), "ms\t\t- metamask transaction:", int(data[3])-int(data[2]), "\t\t- sdatabase update:", int(data[4])-int(data[3]), "ms")
+            #                -owner-         ------- database  read -------         ---- metamask transaction ----         ------ database update  ------         --------- total time ---------
+            f_out.writelines(data[0] + "," + str(int(data[2])-int(data[1])) + "," + str(int(data[3])-int(data[2])) + "," + str(int(data[4])-int(data[3])) + "," + str(int(data[4])-int(data[1])) + "\n")
     f_out.close()
--- server/test/python/sell.py
+++ server/test/python/sell.py
@@ -4,7 +4,6 @@ def convert(lines):
     for line in lines:
         data = line.split(',')
         if len(data) == 7:
-            #                -owner-         -------- input  check --------         -------- upload  ipfs --------         ------ server  response ------         ---- metamask transaction ----         ------ database  upload ------
-            f_out.writelines(data[0] + "," + str(int(data[2])-int(data[1])) + "," + str(int(data[3])-int(data[2])) + "," + str(int(data[4])-int(data[3])) + "," + str(int(data[5])-int(data[4])) + "," + str(int(data[6])-int(data[5])) +"\n")
-            #print(data[0], " - input check:", int(data[2])-int(data[1]), "ms\t\t- upload ipfs:", int(data[3])-int(data[2]), "\t\t- server response:", int(data[4])-int(data[3]), "ms\t\t- metamask transaction", int(data[5])-int(data[4]), "ms\t\t database upload:", int(data[6])-int(data[5]), "ms")
+            #                -owner-         -------- input  check --------         -------- upload  ipfs --------         ------ server  response ------         ---- metamask transaction ----         ------ database  upload ------         --------- total time- --------
+            f_out.writelines(data[0] + "," + str(int(data[2])-int(data[1])) + "," + str(int(data[3])-int(data[2])) + "," + str(int(data[4])-int(data[3])) + "," + str(int(data[5])-int(data[4])) + "," + str(int(data[6])-int(data[5])) + "," + str(int(data[6])-int(data[1])) + "\n")
     f_out.close()
